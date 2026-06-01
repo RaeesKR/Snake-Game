03cda28570909a57c6806595f87c91d4d6bff5c5
teddyirwn <tdyirawan0@gmail.com>
2026-06-01 20:05:41 +0700

    mengubah path index.html

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const path = require("path");
 const app = express();
-app.use(express.static("public"));
-app.listen(3000, () => {
-console.log("Server running di http://localhost:3000");
+app.use("/public", express.static(path.join(__dirname, "public")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
 });
+app.listen(3000, () => {
+  console.log("Server running di http://localhost:3000");
+});
+
+module.exports = app;
